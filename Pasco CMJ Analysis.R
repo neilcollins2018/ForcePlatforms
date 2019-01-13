6b188852340b3c5c2d343856c2d758dbd28817cd
neilcollins2018 <neilcollins87@outlook.com>
2019-01-13 13:39:22 +0000

    New Script

--- Pasco CMJ Analysis.R
+++ Pasco CMJ Analysis.R
@@ -6,20 +6,11 @@ for (i in RequiredPackages) { #Installs packages if not yet installed           
 pacman::p_load("tidyverse","magrittr", 'zoo', 'data.table', 'tidyr', 'devtools',  # 
                "openxlsx" )                                                       #
 ###########Above can be deleted following first error free analysis performed######
-
-####Package load
-library(tidyverse)
-library(magrittr)
-library(zoo)
-library(data.table)
-library(tidyr)
-library(openxlsx)
-
 ##Load functions 
-devtools::source_gist('b966daa2539e10c53a3fc8688d66b819')
+devtools::source_gist('b966daa2539e10c53a3fc8688d66b819', quiet = T)
 
 ###File Read
-data <- PascoDual_read(file=file.choose())
+data <- Pasco_read(file=file.choose())
 
 ##Calculate sample rate
 sample_rate <- sample_rate(data)
@@ -28,50 +19,25 @@ sample_rate <- sample_rate(data)
 list_dataDF <- IND_force_df(data)
 
 ###Calculate Mass
-data_A <- mass_func(list_dataDF[[1]])
-data_C <- mass_func(list_dataDF[[2]])
-data_combined <- mass_func(list_dataDF[[3]])
+data_list <- lapply(list_dataDF, mass_func)
 
 ##Produce weights(N), massess (Kg)
-weight_massA <- weight_mass(data_A)
-weight_massC <- weight_mass(data_C)
-weight_massCombined <- weight_mass(data_combined)
+wght_mass <- lapply(data_list, wght_mass)
 
 ###Produce force, impulse, power, relative values & list of df 
-data_list <- Initial_metric_list(data_A, data_C, data_combined)
+data_list <- Initial_metric_ls(data_list)
 
 ###
 ###Weighing Phase
 list_weighing <- lapply(data_list, function(x){x %<>% filter(Time < 1.5 & Time > 0.5);x})
 
 ####Weights(N)/Masses (Kg)
-list_weights_masses <- weightmass_list(list_weighing)
+list_weights_masses <- wghtmass_lst(list_weighing)
 
-###
-###Unweighting Phase
-list_unweighting <- unweighting_phase(data_list[[1]], data_list[[2]], data_list[[3]])
-
-###
-###Braking Phase
-list_brake <- braking_phase(data_list[[1]], data_list[[2]], data_list[[3]])
-
-###
-###Propulsion Phase
-list_prop <- prop_phase(data_list[[1]], data_list[[2]], data_list[[3]])
-
-###
-###Flight Phase
-list_flight <- flight_phase(data_list[[1]], data_list[[2]], data_list[[3]])
-
-###
-###Landing Phase
-list_landing <- landing_phase(data_list[[1]], data_list[[2]], data_list[[3]])
-
-###Final Wrangling
-#######Seperate into lists of forces/Not phases
-list_A <- listINDforces(1)
-list_C <- listINDforces(2)
-list_combined <- listINDforces(3)
+###Unweighting/Braking/Propulsion/Flight/Landing Phase
+list_A <- phase_split(data_list[[1]])
+list_C <- phase_split(data_list[[2]])
+list_combined <- phase_split(data_list[[3]])
 
 ###Combine Summary Metrics & raw data divided into phases
 finalmetric_df <- finalmetricsdf()
